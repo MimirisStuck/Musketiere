29e86dc747641ba10a9c3d8739804c87e3a0f057
Philipp <philipp.schnake@gmail.com>
2026-03-27 13:12:14 +0100

    nutzerverwaltung anfang

--- user.php
+++ user.php
@@ -4,34 +4,87 @@ Template Name: Nutzer Admin Page
 */
 get_header();
 ?>
+<?php
+    if (!current_user_can('manage_options')) {
+        wp_die('Du hast keine Berechtigung, diese Seite zu sehen.');
+    }
 
-    <table>
+    // Neue Benutzer erstellen
+    if (isset($_POST['new_user_submit'])) {
+        $username = sanitize_user($_POST['username']);
+        $email = sanitize_email($_POST['email']);
+        $password = $_POST['password'];
+        $role = sanitize_text_field($_POST['role']);
+
+        $errors = [];
+
+        if (username_exists($username)) {
+            $errors[] = 'Benutzername existiert bereits.';
+        }
+
+        if (email_exists($email)) {
+            $errors[] = 'E-Mail existiert bereits.';
+        }
+
+        if (empty($password)) {
+            $errors[] = 'Passwort darf nicht leer sein.';
+        }
+
+        if (empty($errors)) {
+            $user_id = wp_create_user($username, $password, $email);
+            wp_update_user(['ID' => $user_id, 'role' => $role]);
+            echo '<div style="color:green;">Neuer Benutzer wurde erstellt!</div>';
+        } else {
+            foreach ($errors as $error) {
+                echo '<div style="color:red;">' . esc_html($error) . '</div>';
+            }
+        }
+    }
+
+    // Alle Benutzer abrufen
+    $users = get_users();
+?>
+
+<h1>Benutzerverwaltung</h1>
+
+<h2>Neue Benutzer erstellen</h2>
+<form method="POST">
+    <label>Benutzername:<br>
+        <input type="text" name="username" required>
+    </label><br><br>
+    <label>E-Mail:<br>
+        <input type="email" name="email" required>
+    </label><br><br>
+    <label>Passwort:<br>
+        <input type="password" name="password" required>
+    </label><br><br>
+    <label>Rolle:<br>
+        <select name="role">
+            <option value="subscriber">Abonnent</option>
+            <option value="editor">Redakteur</option>
+            <option value="author">Autor</option>
+            <option value="administrator">Administrator</option>
+        </select>
+    </label><br><br>
+    <input type="submit" name="new_user_submit" value="Benutzer erstellen">
+</form>
+
+<h2>Alle Benutzer</h2>
+<table border="1" cellpadding="5" cellspacing="0">
     <tr>
         <th>ID</th>
-        <th>Vorname</th>
-        <th>Nachname</th>
-        <th>Email</th>
+        <th>Benutzername</th>
+        <th>E-Mail</th>
         <th>Rolle</th>
     </tr>
-
-    <?php
-    $users = get_musketier_user();
-
-    foreach ($users as $user){
-        echo "<tr>";
-        echo "<td>{$user->id}</td>";
-        echo "<td>{$user->first_name}</td>";
-        echo "<td>{$user->last_name}</td>";
-        echo "<td>{$user->email}</td>";
-        echo "<td>{$user->role_id}</td>";
-        echo "</tr>";
-    }
-    ?>
-
-    </table>
-
-    <div class="special-section">
-        <p>Hier kommt dein individuelles Design hin!</p>
-    </div>
+    <?php foreach ($users as $user): ?>
+    <tr>
+        <td><?php echo $user->ID; ?></td>
+        <td><?php echo esc_html($user->user_login); ?></td>
+        <td><?php echo esc_html($user->user_email); ?></td>
+        <td><?php echo esc_html(implode(', ', $user->roles)); ?></td>
+    </tr>
+    <?php endforeach; ?>
+</table>
 
 <?php get_footer(); ?>
