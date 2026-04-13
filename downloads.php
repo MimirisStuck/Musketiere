fc9a9bc11aec11f594cd3b5f34f1e36517ef6f48
mimirisstuck <philipp.schnake@gmail.com>
2026-04-13 13:04:48 +0200

    test

--- downloads.php
+++ downloads.php
@@ -9,19 +9,18 @@ get_header();
     <h1 class="downloads-title"><?php the_title(); ?></h1>
 
     <?php
-    // PDF-Link als Custom Field oder fester Pfad
-    $pdf_url = get_post_meta(get_the_ID(), 'download_pdf', true); 
-    // Alternativ, fester Pfad:
-    // $pdf_url = get_template_directory_uri() . '/images/Downloadexample.pdf';
+    $image_ids = get_post_meta(get_the_ID(), 'download_images', true);
 
-    // Bild-Vorschau (Featured Image der Page)
-    if (has_post_thumbnail()) {
-        $img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
-    } else {
-        // Fallback-Bild
-        $img_url = get_template_directory_uri() . '/images/pdf-placeholder.png';
-    }
+    if ($image_ids) :
+        $image_ids = explode(',', $image_ids);
     ?>
+        <div class="downloads-gallery">
+            <?php foreach ($image_ids as $id): ?>
+                <?php $url = wp_get_attachment_image_url($id, 'medium'); ?>
+                <img src="<?php echo esc_url($url); ?>" />
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
 
     <a href="<?php echo esc_url($pdf_url); ?>" target="_blank">
         <img src="<?php echo esc_url($img_url); ?>" alt="<?php the_title(); ?>" class="downloads-preview">
