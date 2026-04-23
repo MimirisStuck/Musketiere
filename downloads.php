126e8ee119bc9961e763631d0dba3cd8596acead
mimirisstuck <philipp.schnake@gmail.com>
2026-04-23 14:12:21 +0200

    Download page

--- downloads.php
+++ downloads.php
@@ -8,23 +8,43 @@ get_header();
 <div class="downloads">
     <h1 class="downloads-title"><?php the_title(); ?></h1>
 
-    <?php
-    $image_ids = get_post_meta(get_the_ID(), 'download_images', true);
+    <?php /* echo get_the_content();  */?>
+<?php
 
-    if ($image_ids) :
-        $image_ids = explode(',', $image_ids);
+$content = get_the_content();
+
+preg_match('/href="([^"]+\.pdf)"/i', $content, $matches);
+
+$pdf_url = $matches[1] ?? null;
+
+if ($pdf_url) {
+
+    // Attachment ID der PDF holen
+    $pdf_id = attachment_url_to_postid($pdf_url);
+
+    // Thumbnail der ersten Seite holen
+    $thumbnail = wp_get_attachment_image_url($pdf_id, 'large');
+
     ?>
-        <div class="downloads-gallery">
-            <?php foreach ($image_ids as $id): ?>
-                <?php $url = wp_get_attachment_image_url($id, 'medium'); ?>
-                <img src="<?php echo esc_url($url); ?>" />
-            <?php endforeach; ?>
-        </div>
-    <?php endif; ?>
+    <div class="downloads-pdf-card">
 
-    <a href="<?php echo esc_url($pdf_url); ?>" target="_blank">
-        <img src="<?php echo esc_url($img_url); ?>" alt="<?php the_title(); ?>" class="downloads-preview">
-    </a>
+        <a class="downloads-pdf-link" href="<?php echo $pdf_url; ?>" target="_blank">
+            <?php if ($thumbnail): ?>
+                <img class="downloads-pdf-thumbnail" src="<?php echo $thumbnail; ?>" alt="PDF Vorschau">
+            <?php else: ?>
+                <p>Keine Vorschau verfügbar</p>
+            <?php endif; ?>
+        </a>
+        </br>
+        <a href="<?php echo $pdf_url; ?>" download class="downloads-pdf-btn">
+            Download
+        </a>
+
+    </div>
+    <?php
+}
+?>
+
 </div>
 
 <?php get_footer(); ?>
